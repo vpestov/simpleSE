a05e796305a368571f3741f9822e2514d76027d5
vpestov <valera.hilt@yandex.com>
2014-07-08 18:53:41 +0300

    zipping to archive

--- src/main/java/com/javaSEBasic/ZipHandler.java
+++ src/main/java/com/javaSEBasic/ZipHandler.java
@@ -32,10 +32,8 @@ public class ZipHandler {
     public void unzip(String path) {
         Enumeration entries;
         ZipFile zipFile;
-
         try {
             zipFile = new ZipFile(path);
-
             entries = zipFile.entries();
             zipStructure.addFirst(path);
             ArrayList<String> innerZipContent = new ArrayList<String>();
@@ -116,59 +114,183 @@ public class ZipHandler {
         return Files.probeContentType(path);
     }
 
-    public void zipDirectory(String zipFileName, String dir){
-        final File file = new File(dir);
+//    public void zipDirectory(String zipFileName, String dir){
+//        final File file = new File(dir);
+//        FileOutputStream outputStream = null;
+//        ZipOutputStream zos = null;
+//        try {
+//            outputStream = new FileOutputStream(zipFileName);
+//            zos = new ZipOutputStream(outputStream);
+//            LOGGER.info("creating : {}", zipFileName);
+//            addDirectory(file, zos);
+//        } catch (FileNotFoundException e) {
+//            LOGGER.error("Can't file file : {}", zipFileName);
+//        } finally {
+//            try {
+//                zos.close();
+//            } catch (IOException e) {
+//                LOGGER.error("Can't close OutputStream");
+//            }
+//        }
+//    }
+
+
+    public void createZipArchive (final String zipName,final ArrayList<String> inner){
         FileOutputStream outputStream = null;
         ZipOutputStream zos = null;
         try {
-            outputStream = new FileOutputStream(zipFileName);
+            outputStream = new FileOutputStream(zipName);
             zos = new ZipOutputStream(outputStream);
-            LOGGER.info("creating : {}", zipFileName);
-            addDirectory(file, zos);
+            for (String innerPath: inner){
+                final File innerFile = new File(innerPath);
+                if(innerFile.isDirectory()){
+                    zipDir(innerFile,zos);
+                }else {
+                    zipFile(innerFile,zos);
+                }
+            }
         } catch (FileNotFoundException e) {
-            LOGGER.error("Can't file file : {}", zipFileName);
+            LOGGER.error("Error creating outputStream. File: {} was not found",zipName);
         } finally {
-            try {
-                zos.close();
-            } catch (IOException e) {
-                LOGGER.error("Can't close OutputStream");
+            if (zos != null){
+                try {
+                    zos.close();
+                } catch (IOException e) {
+                    LOGGER.error("Can't close zipOutputStream: {}",zos);
+                }
+            }
+            if (outputStream != null){
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    LOGGER.error("Can't close FileOutputStream: {}",outputStream);
+                }
+
             }
         }
     }
+    public void testMethodToCallZip(){
+        final String path = "D:/testData/testToZip/firstZip.zip";
+        final ArrayList<String> testData = new ArrayList<String>(10);
+        testData.add("D:/testData/testToZip/data2");
+        testData.add("D:/testData/testToZip/data1.txt");
+        createZipArchive(path, testData);
+    }
 
-    private void addDirectory(File dir, ZipOutputStream zos){
+//    public void zipFiles(final String zipName, final ArrayList<String> inner){
+//
+//        try {
+//            final FileOutputStream outputStream = new FileOutputStream(zipName);
+//            final ZipOutputStream zos = new ZipOutputStream(outputStream);
+//            final byte [] buffer = new byte[1024];
+//            FileInputStream fileInputStream = null;
+//
+//            for (String innerPath: inner){
+//                final File innerFile = new File(innerPath);
+//                try {
+//                    fileInputStream = new FileInputStream(innerFile);
+//                    zos.putNextEntry(new ZipEntry(innerFile.getName()));
+//                    final int length = fileInputStream.read(buffer);
+//                    while (length > 0){
+//                        zos.write(buffer,0,length);
+//                    }
+//                    zos.closeEntry();
+//                }catch (FileNotFoundException e) {
+//                    LOGGER.error("File : {} was not found", innerPath);
+//                }catch (IOException e) {
+//                    LOGGER.error("Error Adding zipEntry: {}", innerFile.getName());
+//                }finally {
+//                    try {
+//                        if (fileInputStream!=null){
+//                            fileInputStream.close();
+//                        }
+//                        zos.close();
+//                    } catch (IOException e) {
+//                        LOGGER.error("Can't close ZipOutputStream");
+//                    }
+//                }
+//
+//            }
+//        } catch (FileNotFoundException e) {
+//            LOGGER.error("Error creating outputStream. File: {} was not found",zipName);
+//        }
+//    }
+
+    private void zipDir(File dir, ZipOutputStream zos){
         File[] files = dir.listFiles();
         byte[] tmpBuf = new byte[1024];
-        FileInputStream in = null;
-
         for (File currentFile: files) {
-
             if (currentFile.isDirectory()) {
-                addDirectory(currentFile, zos);
+                zipDir(currentFile, zos);
                 continue;
             }
+                zipFile(currentFile,zos);
+
+
+        }
+    }
+
+    private void zipFile(File file, ZipOutputStream zos) {
+        FileInputStream fileInputStream = null;
+        final byte[] buffer = new byte[1024];
+        try {
+            fileInputStream = new FileInputStream(file);
+            zos.putNextEntry(new ZipEntry(file.getAbsolutePath().replace("D:\\testData\\testToZip\\","")));
+            int length;
+            while ((length = fileInputStream.read(buffer)) > 0) {
+                zos.write(buffer, 0, length);
+            }
+            zos.closeEntry();
+        } catch (FileNotFoundException e) {
+            LOGGER.error("File : {} was not found", file.getAbsolutePath());
+        } catch (IOException e) {
+            LOGGER.error("Error Adding zipEntry: {}", file.getName());
+        } finally {
             try {
-                final String path = currentFile.getAbsolutePath();
-                in = new FileInputStream(path);
-                zos.putNextEntry(new ZipEntry(path));
-                int len;
-                while ((len = in.read(tmpBuf)) > 0) {
-                    zos.write(tmpBuf, 0, len);
+                if (fileInputStream != null) {
+                    fileInputStream.close();
                 }
-            } catch (FileNotFoundException e) {
-                LOGGER.error("File : {} was not found ", currentFile.getAbsolutePath());
             } catch (IOException e) {
-                LOGGER.error("Can't create Zip Entry : {}",currentFile.getAbsolutePath());
-            } finally {
-                try {
-                    zos.closeEntry();
-                    in.close();
-                } catch (IOException e) {
-                    LOGGER.error("can't close Entry/inputStream");
-                }
+                LOGGER.error("Can't close ZipOutputStream");
             }
         }
     }
+
+//    private void addDirectory(File dir, ZipOutputStream zos){
+//        File[] files = dir.listFiles();
+//        byte[] tmpBuf = new byte[1024];
+//        FileInputStream in = null;
+//
+//        for (File currentFile: files) {
+//
+//            if (currentFile.isDirectory()) {
+//                addDirectory(currentFile, zos);
+//                continue;
+//            }
+//            try {
+//                final String path = currentFile.getAbsolutePath();
+//                in = new FileInputStream(path);
+//                zos.putNextEntry(new ZipEntry(path));
+//                int len;
+//                while ((len = in.read(tmpBuf)) > 0) {
+//                    zos.write(tmpBuf, 0, len);
+//                }
+//                zos.closeEntry();
+//            } catch (FileNotFoundException e) {
+//                LOGGER.error("File : {} was not found ", currentFile.getAbsolutePath());
+//            } catch (IOException e) {
+//                LOGGER.error("Can't create Zip Entry : {}",currentFile.getAbsolutePath());
+//            } finally {
+//                try {
+//                    if(in!=null){
+//                        in.close();
+//                    }
+//                } catch (IOException e) {
+//                    LOGGER.error("can't close inputStream");
+//                }
+//            }
+//        }
+//    }
 
     private void copyInputStream(InputStream in, OutputStream out) throws IOException {
         byte[] buffer = new byte[1024];
